2628e753cae58d9f6e65c5aa17a826b8812c46f8
Yulia Solovenuk <121509951+YuliyaSolovenuk@users.noreply.github.com>
2023-05-21 13:08:35 +0300

    task-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,2 +1,36 @@
 
-hello
+const BtnStartEl = document.querySelector('[data-start]')
+console.log(BtnStartEl)
+
+const BtnStopEl = document.querySelector('[data-stop]')
+console.log(BtnStopEl)
+
+const bodyEl = document.body;
+console.log(bodyEl)
+
+let timerId = null;
+disabled = false;
+
+BtnStartEl.addEventListener('click', onChangeBgcolor)
+
+BtnStopEl.addEventListener('click', offChangeBgcolor)
+
+
+
+function onChangeBgcolor(e) {
+    timerId = setInterval(() => {
+        bodyEl.style.backgroundColor = getRandomHexColor()
+        
+    }, 1000)
+    BtnStartEl.disabled = true;
+}
+
+function offChangeBgcolor(e) {
+    clearInterval(timerId);
+    BtnStartEl.disabled = false;
+}
+
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
