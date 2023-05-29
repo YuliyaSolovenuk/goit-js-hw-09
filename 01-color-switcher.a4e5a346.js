7c7019e1639e1a5a3c8803e8fe68e5b38cba3657
YuliyaSolovenuk <121509951+YuliyaSolovenuk@users.noreply.github.com>
2023-05-29 11:21:12 +0000

    Deploying to gh-pages from @ YuliyaSolovenuk/goit-js-hw-09@a85464f5777f89429755990885421824a3b8407c 🚀

---
+++ 01-color-switcher.a4e5a346.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),a=document.body,d=null;disabled=!1,t.addEventListener("click",(function(n){d=setInterval((function(){a.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3),t.disabled=!0,e.disabled=!1})),e.addEventListener("click",(function(a){clearInterval(d),t.disabled=!1,e.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.a4e5a346.js.map
